931142a710718c5f77d98fe5c9951eb386f42aa5
gemj98 <gemj98@gmail.com>
2024-11-12 14:12:53 -0800

    Model training

--- test_code/modeling.py
+++ test_code/modeling.py
@@ -9,7 +9,7 @@ from tensorflow.keras.utils import image_dataset_from_directory
 
 script_dir = os.path.dirname(__file__)
 data_dir = os.path.join(script_dir, 'data', 'PKLot', 'PKLot_cropped')
-model_path = os.path.join(script_dir, 'model.h5')
+model_path = os.path.join(script_dir, 'model.keras')
 
 batch_size = 32
 img_height = 30
@@ -21,7 +21,8 @@ train_ds = image_dataset_from_directory(
   subset="training",        
   seed=123,
   image_size=(img_height, img_width),
-  batch_size=batch_size)
+  batch_size=batch_size,
+  color_mode="grayscale")
 
 val_ds = image_dataset_from_directory(
   data_dir,
@@ -29,14 +30,15 @@ val_ds = image_dataset_from_directory(
   subset="validation",
   seed=123,
   image_size=(img_height, img_width),
-  batch_size=batch_size)
+  batch_size=batch_size,
+  color_mode="grayscale")
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
 # Model architecture
 model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
+    Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 1)),
     MaxPooling2D(2, 2),
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D(2, 2),
@@ -49,7 +51,7 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-epochs = 2
+epochs = 3
 history = model.fit(
     train_ds,
     validation_data=val_ds,
